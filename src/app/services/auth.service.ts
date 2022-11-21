082803d7f4049aac3eb99a99baa4166a934eda9c
fermin <fermin.unicen@gmail.com>
2022-11-21 09:30:19 -0300

    se hizo la conexion con firebase, se agrego un guard para el dashboard con auttenticacion de usuario desde firebase

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
+import { map, tap } from 'rxjs';
+import { Usuario } from '../models/usuario.models';
 
 
 @Injectable({
@@ -8,12 +12,29 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 export class AuthService {
 
   constructor(
-    public fireAuth: AngularFireAuth
+    public fireAuth: AngularFireAuth,
+    public fireStore: AngularFirestore,
+    private router: Router
   ) { }
 
+  initAuthListener(){
+    this.fireAuth.authState.subscribe(fuser => {
+      console.log({
+        1: fuser?.email,
+        2: fuser?.displayName,
+        3: fuser?.uid
+      })
+    })
+  }
 
   crearUsuario( nombre:string, email:string, password:string ){
     return this.fireAuth.createUserWithEmailAndPassword(email, password)
+            .then( ({user}) => {
+              const newUser = new Usuario( user?.uid!, nombre, user?.email! );
+
+              return this.fireStore.doc(`${user?.uid}/usuario`)
+              .set({...newUser})
+            })
   }
   
   iniciarSesion( email:string, password:string ){
@@ -22,5 +43,14 @@ export class AuthService {
 
   cerrarSesion(){
     return this.fireAuth.signOut();
+  }
+
+  isAuth(){
+    return this.fireAuth.authState.pipe(
+      map( fireUser => fireUser ? true : false ),
+      tap( isAuth => {
+        !isAuth ? this.router.navigateByUrl('/login') : null 
+      })
+    );
   }
 }
